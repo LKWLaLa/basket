9b4f9ca20fc732dde371aa22ef7c2a92557af540
LKWLaLa <lkwlala@gmail.com>
2018-09-18 18:22:22 -0400

    add basket items to state and ability to clear in-basket items from list

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -11,7 +11,8 @@ class ListContainer extends Component {
         {name: "oranges", quantity: 3, inBasket: false, id: 1},
         {name: "fancy cheese", quantity: 2, inBasket: false, id: 2},
         {name: "milk", quantity: 1, inBasket: false, id: 3}
-      ]
+      ],
+      basket: []
     }
   }
 
@@ -30,22 +31,32 @@ class ListContainer extends Component {
   }
 
   toggleInBasket = (id) => {
-    let newItems = [...this.state.items].map(item => {
+    let newItems = this.state.items.map(item => {
       if(item.id === parseInt(id)){
         item.inBasket = !item.inBasket
       }
       return item
     }) 
-    this.setState({ items: newItems})
+    let basketItems = this.state.items.filter(
+      item => item.inBasket === true)
+    this.setState({ items: newItems, basket: basketItems})
   }
+
+  clearInBasket = () => {
+      let nonBasketItems = this.state.items.filter(
+      item => item.inBasket === false)
+      this.setState({ items: nonBasketItems, showInBasket: false})
+  }
+
+
 
   render(){
     return (
       <div className="list-container">
         <h1>Grocery List</h1>
         <AddItem addNewItem={this.addNewItem}/>
-        <button className="clear-basket-items">
-          Clear all 'in basket' items
+        <button className="clear-basket-items" onClick={this.clearInBasket}>
+          Clear in-basket items
         </button>
         <List items={this.state.items} deleteItem={this.deleteItem}
           toggleInBasket={this.toggleInBasket}/>
